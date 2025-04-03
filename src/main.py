d4e8014407c2079bf0b5b8d5939891a54ee5591a
Adrian Hernandez Padron <adrianhdezp10@gmail.com>
2025-04-03 23:04:16 +0100

    Cambios en la organizacion de los ficheros

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
-from PyQt5.QtWidgets import QApplication
-from gui import FitsViewer
 import sys
+from PyQt5.QtWidgets import QApplication
+from gui.main_window import FitsViewer
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     viewer = FitsViewer()
     viewer.show()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
