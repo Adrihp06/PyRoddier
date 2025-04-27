05b1ba61cebda2b1603081b5c3dd269cb65906d6
Adrian Hernandez Padron <adrianhdezp10@gmail.com>
2025-04-27 22:09:33 +0100

    Normalize by the max in the psf

--- src/core/psf.py
+++ src/core/psf.py
@@ -1,27 +1,12 @@
 # Copyright (c) 2025 Adrián Hernández Padrón
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
-
-import numpy as np
-
 import numpy as np
 
 def calculate_psf(W_rec, pupila_mask):
-    # Convertir frente de onda en radianes
     fase_W = 2 * np.pi * W_rec
-
-    # Función pupilar compleja
     pupil_function = pupila_mask * np.exp(1j * fase_W)
-
-    # FFT normalizada correctamente para generar la PSF
     E_focal = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(pupil_function)))
-
-    # Intensidad (PSF)
     PSF = np.abs(E_focal)**2
-
-    # Normalización para visualización correcta
-    PSF /= PSF.sum()
-
-    # Versión logarítmica para visualización
+    PSF /= PSF.max()
     PSF_log = np.log10(PSF + 1e-8)
-
     return PSF, PSF_log
