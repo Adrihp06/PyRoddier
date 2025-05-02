c9604d5459f29e27b90bba6cc58f2e7372340a28
Adrian Hernandez Padron <adrianhdezp10@gmail.com>
2025-05-02 08:19:45 +0100

    Change wavefront in mm to rad

--- src/core/psf.py
+++ src/core/psf.py
@@ -2,8 +2,9 @@
 # Licensed under the MIT License. See LICENSE file in the project root for full license information.
 import numpy as np
 
-def calculate_psf(wavefront, pupila_mask, wavelength = 555e-6):
-    fase_W = 2 * np.pi * wavefront / 1000
+def calculate_psf(wavefront, pupila_mask, wavelength = 556):
+    rad =  (2 * np.pi / wavelength)
+    fase_W = 2 * np.pi * wavefront * rad
     pupil_function = pupila_mask * np.exp(1j * fase_W)
     E_focal = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(pupil_function)))
     PSF = np.abs(E_focal)**2
